25c0bf048e96caaeb4cbb5295795d562dea851ca
liupan010 <liupan010@ke.com>
2018-11-15 18:04:02 +0800

    1115

--- src/main/java/com/example/springboot/springboot/ReadingListRepository.java
+++ src/main/java/com/example/springboot/springboot/ReadingListRepository.java
@@ -2,6 +2,8 @@ package com.example.springboot.springboot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReadingListRepository extends JpaRepository<Book, Long> {
-	
+	List<Book> findByReader(String reader);
 }
